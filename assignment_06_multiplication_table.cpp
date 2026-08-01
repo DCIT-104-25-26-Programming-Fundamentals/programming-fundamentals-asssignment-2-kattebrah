65931c1e9efd43426c395536b11e223dea7165d2
SAINt Mel_ <kattebrah@st.ug.edu.gh>
2026-08-01 16:40:35 +0000

    multiplication table

--- assignment_06_multiplication_table.cpp
+++ assignment_06_multiplication_table.cpp
@@ -57,3 +57,54 @@
 #include <iostream>
 using namespace std;
 
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void printSingleTable(int num) {
+    cout << "Multiplication Table for " << num << ":" << endl;
+    for (int i = 1; i <= 12; i++) {
+        cout << num << " x " << setw(2) << i << " = " << num * i << endl;
+    }
+}
+
+void printTablesUpToN(int n) {
+    if (n <= 0) {
+        cout << "Error: N must be a positive integer." << endl;
+        return;
+    }
+
+    for (int num = 1; num <= n; num++) {
+        printSingleTable(num);
+        if (num != n) {
+            cout << "---------------------------" << endl;
+        }
+    }
+}
+
+int main() {
+    int choice;
+    cout << "Multiplication Table Generator" << endl;
+    cout << "1. Single Table" << endl;
+    cout << "2. Tables from 1 to N" << endl;
+    cout << "Enter your choice: ";
+    cin >> choice;
+
+    if (choice == 1) {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        printSingleTable(num);
+
+    } else if (choice == 2) {
+        int n;
+        cout << "Enter a number N: ";
+        cin >> n;
+        printTablesUpToN(n);
+
+    } else {
+        cout << "Invalid choice." << endl;
+    }
+
+    return 0;
+}
